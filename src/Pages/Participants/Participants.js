eba2f4ca0ecb7cf5251bdf6c4554635fdb71acb2
SharminAkter3 <mssharmin4145@gmail.com>
2023-01-11 15:52:29 +0600

    add navbar/sharmin

---
+++ src/Pages/Participants/Participants.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Participants = () => {
+    return (
+        <div>
+            <h2>This is participants page.</h2>
+        </div>
+    );
+};
+
+export default Participants;
