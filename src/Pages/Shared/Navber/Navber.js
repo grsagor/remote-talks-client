4d00d8185aadbb85c119ad0ba65a14d9b34e3790
SharminAkter3 <mssharmin4145@gmail.com>
2023-01-12 17:08:12 +0600

    update navbar

--- src/Pages/Shared/Navber/Navber.js
+++ src/Pages/Shared/Navber/Navber.js
@@ -15,7 +15,32 @@ const Navbar = () => {
       <div className="navbar h-20 mb-12">
         <div className="flex-1">
 
-          <div className="dropdown">
+          <div className='flex-1'>
+            <a href='/' className="btn btn-ghost font-bold text-2xl normal-case">LOGO</a>
+          </div>
+
+          <div className='hidden lg:flex flex-1'>
+            <div className="form-control flex justify-center">
+              <input type="text" placeholder="Search" className="input input-bordered border-secondary h-12 w-80 rounded-3xl" />
+            </div>
+          </div>
+        </div>
+
+        <div className='lg:hidden'>
+          <div className="dropdown dropdown-left mt-4">
+            <button className="btn btn-ghost btn-circle">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mb-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+            </button>
+            <ul tabIndex={0} className="flex dropdown-content">
+              <div className="form-control flex justify-center">
+                <input type="text" placeholder="Search" className="input input-bordered border-secondary h-8 w-60 rounded-3xl" />
+              </div>
+            </ul>
+          </div>
+        </div>
+
+        <div className="dropdown mr-2">
+          <div className="dropdown-end">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
@@ -29,7 +54,6 @@ const Navbar = () => {
 
               <Link className='m-2 text-lg' to='/'>Profile</Link>
               {menuItems}
-              <Link><button className='m-2 text-lg'>Theme</button></Link>
               <Link className='m-2 text-lg' to='/'>Setting</Link>
               <Link className='m-2 text-lg' to='/logOut'>Log Out</Link>
 
@@ -37,29 +61,26 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            {menuItems}
-          </ul>
-        </div>
-
-        <div className="navbar-end">
-          <div className='flex lg:hidden'>
-            <button className="btn btn-ghost btn-circle">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-            </button>
-          </div>
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar mr-5">
-              <div className="w-10 rounded-full ">
-                <img src="https://i.pinimg.com/originals/de/99/93/de9993e752fc52646579448542c411d3.jpg" alt='' />
-              </div>
-            </label>
-            <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-              <li><a href='/'>Profile</a></li>
-              <li><a href='/'>Settings</a></li>
-              <li><a href='/logOut'>Log Out</a></li>
+        <div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              {menuItems}
             </ul>
+          </div>
+
+          <div className="navbar-end hidden lg:flex">
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full ">
+                  <img src="https://i.pinimg.com/originals/de/99/93/de9993e752fc52646579448542c411d3.jpg" alt='' />
+                </div>
+              </label>
+              <ul tabIndex={0} className="shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                <li><a href='/'>Profile</a></li>
+                <li><a href='/'>Settings</a></li>
+                <li><a href='/logOut'>Log Out</a></li>
+              </ul>
+            </div>
           </div>
         </div>
 
