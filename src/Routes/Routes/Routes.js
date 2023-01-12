a5a81c22ca78061fd94f6d3ac4052e41b8528cc6
anichu <anis.cse5.bu@gmail.com>
2023-01-12 16:40:35 +0600

    added feedback section in home page

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,45 +1,48 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "../../Layout/Main/Main";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import LogOut from "../../Pages/LogOut/LogOut";
 import Message from "../../Pages/Message/Message";
 import Participants from "../../Pages/Participants/Participants";
+import FeedBack from "../../Pages/Shared/FeedBack/FeedBack";
 import SignUp from "../../Pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Main></Main>,
-    children: [
-      {
-        path: '/',
-        element: <Home></Home>
-      },
-      {
-        path: '/home',
-        element: <Home></Home>
-      },
-      {
-        path: '/participants',
-        element: <Participants></Participants>
-      },
-      {
-        path: '/message',
-        element: <Message></Message>
-      },
-      {
-        path: '/login',
-        element: <Login></Login>
-      },
-      {
-        path: '/logOut',
-        element: <LogOut></LogOut>
-      },
-    ]
-  }
+	{
+		path: "/",
+		element: <Main></Main>,
+		children: [
+			{
+				path: "/",
+				element: (
+					<>
+						<Home></Home>
+						<FeedBack></FeedBack>
+					</>
+				),
+			},
+			{
+				path: "/home",
+				element: <Home></Home>,
+			},
+			{
+				path: "/participants",
+				element: <Participants></Participants>,
+			},
+			{
+				path: "/message",
+				element: <Message></Message>,
+			},
+			{
+				path: "/login",
+				element: <Login></Login>,
+			},
+			{
+				path: "/logOut",
+				element: <LogOut></LogOut>,
+			},
+		],
+	},
 ]);
 export default router;
