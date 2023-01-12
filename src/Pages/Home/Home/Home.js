65e62793fefa6cb991d7667fa59aabdbf8867b51
sultanmahmud07 <asadkhan01862@gmail.com>
2023-01-13 00:33:42 +0600

    features section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Features from "../Features/Features";
 import FeedBack from "../FeedBack/FeedBack";
 
 const Home = () => {
 	return (
 		<div>
+      <Features></Features>
 			<FeedBack></FeedBack>
 		</div>
 	);
