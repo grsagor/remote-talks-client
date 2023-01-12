72de0f92d4e6ee5ff655494e561a158f50f2683a
anichu <anis.cse5.bu@gmail.com>
2023-01-12 18:40:55 +0600

    added responsive design

--- src/Pages/Home/FeedBack/FeedBack.js
+++ src/Pages/Home/FeedBack/FeedBack.js
@@ -4,20 +4,21 @@ import "./Feedback.css";
 const FeedBack = () => {
 	return (
 		<div className="feedback__container w-[90%] pt-[15px] mx-auto">
-			<h1 className="py-5 text-2xl text-center capitalize sm:text-3xl feedback__title">
-				We'd love to know what you think
+			<h1 className="px-5 py-5 text-2xl text-center capitalize sm:px-0 sm:text-3xl feedback__title">
+				We'd love to know what{" "}
+				<br className="block sm:hidden feedback__break" /> you think
 			</h1>
 			<div className="feedback__subcontainer pt-[10px]">
 				<form action="" className="w-[80%] mx-auto">
-					<div className="flex justify-center w-full py-5">
-						<div className="w-[50%] mr-10">
+					<div className="flex flex-col justify-center w-full py-5 sm:flex-row">
+						<div className="sm:w-[50%] w-full sm:mr-10 mr-0">
 							<input
 								type="text"
 								className="w-full px-1 py-2 text-black border-[1px] rounded-md outline-none feedback__input"
 								placeholder="first name...."
 							/>
 						</div>
-						<div className="w-[50%]">
+						<div className="sm:w-[50%] sm:pt-0 pt-10 w-full">
 							<input
 								type="text"
 								className="w-full px-1 py-2 text-black border-[1px] rounded-md outline-none feedback__input"
@@ -45,7 +46,7 @@ const FeedBack = () => {
 						</div>
 					</div>
 					<div className="py-5 text-center ">
-						<button className="px-5 py-3 capitalize rounded-md feedback__button">
+						<button className="px-5 py-3 capitalize transition-all duration-500 ease-in-out rounded-md feedback__button">
 							send feedback
 						</button>
 					</div>
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,11 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "../../Layout/Main/Main";
+import FeedBack from "../../Pages/Home/FeedBack/FeedBack";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import LogOut from "../../Pages/LogOut/LogOut";
 import Message from "../../Pages/Message/Message";
 import Participants from "../../Pages/Participants/Participants";
-import FeedBack from "../../Pages/Shared/FeedBack/FeedBack";
+
 import SignUp from "../../Pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
