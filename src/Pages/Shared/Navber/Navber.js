b22ff0e5d4f2789ff4c8c28d8a89bee719eb050a
SharminAkter3 <mssharmin4145@gmail.com>
2023-01-12 14:37:39 +0600

    update navbar/sharmin

--- src/Pages/Shared/Navber/Navber.js
+++ src/Pages/Shared/Navber/Navber.js
@@ -14,25 +14,27 @@ const Navbar = () => {
     <div>
       <div className="navbar h-20 mb-12">
         <div className="flex-1">
+
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
+
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <label tabIndex={0} className="flex btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full ">
+                  <img src="https://i.pinimg.com/originals/de/99/93/de9993e752fc52646579448542c411d3.jpg" alt='' />
+                </div>
+              </label>
+
+              <Link className='m-2 text-lg' to='/'>Profile</Link>
               {menuItems}
+              <Link><button className='m-2 text-lg'>Theme</button></Link>
+              <Link className='m-2 text-lg' to='/'>Setting</Link>
+              <Link className='m-2 text-lg' to='/logOut'>Log Out</Link>
+
             </ul>
           </div>
-
-          <div>
-            <a href='/' className="btn btn-ghost font-bold text-2xl normal-case">LOGO</a>
-          </div>
-
-          <div className='hidden lg:flex'>
-            <div className="form-control flex justify-center px-60 ml-56 ">
-              <input type="text" placeholder="Search" className="input input-bordered border-secondary h-12 py-0 w-80 rounded-3xl" />
-            </div>
-          </div>
-
         </div>
 
         <div className="navbar-center hidden lg:flex">
@@ -60,6 +62,7 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
+
       </div>
     </div>
   );
