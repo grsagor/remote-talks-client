88c605518be110745740415d1ec0e703d3229139
anichu <anis.cse5.bu@gmail.com>
2023-01-12 23:35:15 +0600

    added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>
-      <h1 className='py-40'>This is home page!!</h1>
-     
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="py-40">This is home page!!</h1>
+		</div>
+	);
 };
 
 export default Home;
