b1807ee6cfb9c7d1546519a587896a61ce771fbb
nusratnova5 <nusrat.nova16@gmail.com>
2023-01-17 20:56:02 +0600

    commit to pull

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
-import img from '../Login/images/login.png';
 
 const Login = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -14,36 +13,29 @@ const Login = () => {
 
   }
   return (
-    <div className='grid grid-cols-4 mx-auto my-4 gap-5 '>
-      <div className='col-span-2 w-2/4 mx-auto bg-violet-400 p-5 text-black rounded'>
-        <form onSubmit={handleSubmit(handleLogin)}>
+    <div className='w-3/4 mx-auto my-4'>
+      <form onSubmit={handleSubmit(handleLogin)}>
 
-          <div className="form-control w-full ">
-            <label className="label"><span className="label-text text-black font-bold">Email</span></label>
-            <input {...register("email", { required: "Email Address is required" })} type="text" placeholder="Enter your email" className="input input-bordered w-full" />
-            {errors.email && <p className='text-error'>{errors.email?.message}</p>}
-          </div>
+        <div className="form-control w-full">
+          <label className="label"><span className="label-text">Email</span></label>
+          <input {...register("email", { required: "Email Address is required" })} type="text" placeholder="Enter email" className="input input-bordered w-full" />
+          {errors.email && <p className='text-error'>{errors.email?.message}</p>}
+        </div>
 
-          <div className="form-control w-full ">
-            <label className="label"><span className="label-text text-black font-bold">Password</span></label>
-            <input {...register("password", { required: "Password is required" })} type="password" placeholder="Enter  your password" className="input input-bordered w-full" />
-            {errors.password && <p className='text-error'>{errors.password?.message}</p>}
-            <label className="label"><span className="label-text text-black font-bold">Forgot Password?</span></label>
-          </div>
+        <div className="form-control w-full">
+          <label className="label"><span className="label-text">Password</span></label>
+          <input {...register("password", { required: "Password is required" })} type="password" placeholder="Enter password" className="input input-bordered w-full" />
+          {errors.password && <p className='text-error'>{errors.password?.message}</p>}
+          <label className="label"><span className="label-text">Forgot Password?</span></label>
+        </div>
 
-          <input className='btn btn-primary w-full font-bold' value='Login' type="submit" />
-        </form>
+        <input className='btn btn-primary w-full' value='Login' type="submit" />
+      </form>
 
-        <p className='pt-1'>New to the website!please <Link className='font-semibold' to='/signup'>SIGN UP</Link> first.</p>
+      <p>If you don't have any account, please <Link className='font-semibold' to='/signup'>Sign Up</Link>.</p>
 
 
-        <button onClick={handleGoogleSignIn} className='btn btn-outline w-full my-4 text-black'>Continue With Google</button>
-      </div>
-      <div className='col-span-2 w-3/4'>
-         <img src={img}></img>
-      </div>
-      
-
+      <button onClick={handleGoogleSignIn} className='btn btn-outline w-full my-4'>Continue With Google</button>
     </div>
   );
 };
