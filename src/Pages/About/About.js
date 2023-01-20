5a091049eca28e51085986fb84ec8e4ddd6ea646
SharminAkter3 <mssharmin4145@gmail.com>
2023-01-20 16:42:28 +0600

    update

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className='text-center flex flex-col items-center my-4'>
-            <h1 className='font-bold text-4xl border-set p-2 mb-4'>Team: Hell-Bent Developers </h1>
+            <h1 className='font-bold text-4xl border-set p-2 mb-4'>Meet Our Team </h1>
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5 gap-8'>
                 <div className='card w-50 h-100 text-center hover:bg-base-300 px-5 py-10 shadow-2xl'>
