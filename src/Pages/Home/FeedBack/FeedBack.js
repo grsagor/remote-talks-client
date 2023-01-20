a80f9310dca9d30e4f732160d5984c72078b93f3
anichu <anis.cse5.bu@gmail.com>
2023-01-20 20:31:11 +0600

    added feeback

--- src/Pages/Home/FeedBack/FeedBack.js
+++ src/Pages/Home/FeedBack/FeedBack.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { useAsyncError } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./Feedback.css";
 
 const FeedBack = () => {
@@ -26,68 +27,97 @@ const FeedBack = () => {
 			.then((res) => res.json())
 			.then((data) => {
 				form.reset();
+				if (data.acknowledged) {
+					toast.success("Your feedBack is sent", {
+						position: "bottom-right",
+						autoClose: 5000,
+						hideProgressBar: false,
+						closeOnClick: true,
+						pauseOnHover: true,
+						draggable: true,
+						progress: undefined,
+						theme: "colored",
+					});
+				}
 			})
 			.catch((err) => {
 				console.log(err);
 			});
 	};
 	return (
-		<div className="feedback__container w-[90%] pt-[15px] mx-auto">
-			<h1 className="px-5 py-5 text-2xl text-center capitalize sm:px-0 sm:text-3xl feedback__title">
-				We'd love to know what{" "}
-				<br className="block sm:hidden feedback__break" /> you think
-			</h1>
-			<div className="feedback__subcontainer pt-[10px]">
-				<form onSubmit={submitHandler} className="w-[80%] mx-auto">
-					<div className="flex flex-col justify-center w-full py-5 sm:flex-row">
-						<div className="sm:w-[50%] w-full sm:mr-10 mr-0">
-							<input
-								type="text"
-								name="firstname"
-								className="w-full px-1 py-2 text-black border-[1px] rounded-md outline-none feedback__input"
-								placeholder="first name...."
-							/>
-						</div>
-						<div className="sm:w-[50%] sm:pt-0 pt-10 w-full">
-							<input
-								type="text"
-								name="lastname"
-								className="w-full px-1 py-2 text-black border-[1px] rounded-md outline-none feedback__input"
-								placeholder="last name....."
-							/>
-						</div>
-					</div>
+		<>
+			<ToastContainer
+				position="bottom-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="colored"
+			/>
+			{/* Same as */}
+			<ToastContainer />
 
-					<div className="flex justify-center w-full py-5">
-						<div className="w-full">
-							<input
-								type="email"
-								name="email"
-								className="w-full px-1 py-2 text-black border-[1px] rounded-md outline-none feedback__input"
-								placeholder="example@gmail.com...."
-							/>
+			<div className="feedback__container w-[90%] pt-[15px] mx-auto">
+				<h1 className="px-5 py-5 text-2xl text-center capitalize sm:px-0 sm:text-3xl feedback__title">
+					We'd love to know what{" "}
+					<br className="block sm:hidden feedback__break" /> you think
+				</h1>
+				<div className="feedback__subcontainer pt-[10px]">
+					<form onSubmit={submitHandler} className="w-[80%] mx-auto">
+						<div className="flex flex-col justify-center w-full py-5 sm:flex-row">
+							<div className="sm:w-[50%] w-full sm:mr-10 mr-0">
+								<input
+									type="text"
+									name="firstname"
+									className="w-full px-1 py-2 text-black border-[1px] rounded-md outline-none feedback__input"
+									placeholder="first name...."
+								/>
+							</div>
+							<div className="sm:w-[50%] sm:pt-0 pt-10 w-full">
+								<input
+									type="text"
+									name="lastname"
+									className="w-full px-1 py-2 text-black border-[1px] rounded-md outline-none feedback__input"
+									placeholder="last name....."
+								/>
+							</div>
 						</div>
-					</div>
-					<div className="flex justify-center w-full py-5">
-						<div className="w-full">
-							<textarea
-								name="message"
-								placeholder="How can we Improve?"
-								className="w-full px-3 py-2 rounded-md outline-none feedback__input"
-							></textarea>
+
+						<div className="flex justify-center w-full py-5">
+							<div className="w-full">
+								<input
+									type="email"
+									name="email"
+									className="w-full px-1 py-2 text-black border-[1px] rounded-md outline-none feedback__input"
+									placeholder="example@gmail.com...."
+								/>
+							</div>
 						</div>
-					</div>
-					<div className="py-5 text-center ">
-						<button
-							type="submit"
-							className="px-5 py-3 capitalize transition-all duration-500 ease-in-out rounded-md feedback__button"
-						>
-							send feedback
-						</button>
-					</div>
-				</form>
+						<div className="flex justify-center w-full py-5">
+							<div className="w-full">
+								<textarea
+									name="message"
+									placeholder="How can we Improve?"
+									className="w-full px-3 py-2 rounded-md outline-none feedback__input"
+								></textarea>
+							</div>
+						</div>
+						<div className="py-5 text-center ">
+							<button
+								type="submit"
+								className="px-5 py-3 capitalize transition-all duration-500 ease-in-out rounded-md feedback__button"
+							>
+								send feedback
+							</button>
+						</div>
+					</form>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
