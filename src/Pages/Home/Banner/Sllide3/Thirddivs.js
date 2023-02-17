10fe47d8479d3ede59b2413dcf9377a068c8d695
devSobuj910 <ftsobuj222@gmail.com>
2023-02-17 11:21:22 +0600

    home banner animation and mesejer change

---
+++ src/Pages/Home/Banner/Sllide3/Thirddivs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import presntion from "./prsention.jpeg"
+const Thirddivs = () => {
+    return (
+      <div>
+        <img className='rounded-lg' src={presntion} alt="" />
+      </div>
+    );
+};
+
+export default Thirddivs;
---
+++ src/Pages/Home/Banner/slide4/Fourthdivs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import videoconfarence from "./videoconfarenchi.jpg"
+const Fourthdivs = () => {
+    return (
+      <div>
+        <img src={videoconfarence}  className="rounded-lg" alt="" />
+      </div>
+    );
+};
+
+export default Fourthdivs;
